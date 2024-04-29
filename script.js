0d7570cfa8a59109c77b7f69bb9064dbc72d0965
sliverhearer <tianyi@woaini.com>
2024-04-29 09:52:46 +0800

    test

---
+++ script.js
@@ -0,0 +1,16 @@
+// script.js  
+  
+// document.getElementById('home-link').addEventListener('click', function(event) {  
+//     event.preventDefault(); // 阻止默认的链接跳转行为  
+//     document.getElementById('content').innerHTML = 'This is the home page content.'; // 更新内容区域的内容  
+// });  
+  
+// document.getElementById('about-link').addEventListener('click', function(event) {  
+//     event.preventDefault(); // 阻止默认的链接跳转行为  
+//     document.getElementById('content').innerHTML = 'This is the about page content.'; // 更新内容区域的内容  
+// });  
+  
+// 为其他链接添加类似的事件监听器...
+tianyi(){
+	url:href="https://mzh.moegirl.org.cn/%E6%B4%9B%E5%A4%A9%E4%BE%9D";
+}
